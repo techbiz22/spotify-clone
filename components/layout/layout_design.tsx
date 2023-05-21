c7e05c18d5116dd9caa95726cc1453813cab1a88
techbiz22 <techbiz22@gmail.com>
2023-05-21 12:08:17 +0100

    first commit

---
+++ components/layout/layout_design.tsx
@@ -0,0 +1,10 @@
+// import React from 'react'
+
+const LayoutDesignPage:React.FC<React.PropsWithChildren> = ({children})=>{
+  return(
+    <div>Layout design</div>
+  )
+}
+
+export default LayoutDesignPage
+
